baf587ca638f6cc824b0c13d3a4d0d3426e202c4
Javier López <graulopezjavier@gmail.com>
2022-05-03 18:40:52 -0500

    feat: kraken initial setup

---
+++ features/web/support/hooks.js
@@ -0,0 +1,13 @@
+const { After, Before } = require('@cucumber/cucumber');
+const { WebClient } = require('kraken-node');
+
+
+Before(async function() {
+  this.deviceClient = new WebClient('chrome', {}, this.userId);
+  this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
+  this.testId = this.userId;
+})
+
+After(async function() {
+  await this.deviceClient.stopKrakenForUserId(this.userId);
+});
